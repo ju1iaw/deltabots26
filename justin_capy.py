c6c255a503610642b8083bdc1e187535bc63fa1c
justinwang636-sys <justinwang636@gmail.com>
2026-06-28 12:12:17 -0700

    Update justin_capy.py
    turn 90 degrees

--- justin_capy.py
+++ justin_capy.py
@@ -5,6 +5,7 @@ from base_robot import *
 def Run(br: BaseRobot):
     br.moveLeftAttachmentMotorForMillis(millis=500, speed=250)
     br.driveForDistance(600, 200)
+    br.turnForAngle(90, speed=100)
     
 
 
