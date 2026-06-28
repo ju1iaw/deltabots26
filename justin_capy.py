85d2b898cb9449d7804a2dc0674eeb5e82ffea20
justinwang636-sys <justinwang636@gmail.com>
2026-06-28 12:14:51 -0700

    Update justin_capy.py
    faster turn

--- justin_capy.py
+++ justin_capy.py
@@ -5,7 +5,7 @@ from base_robot import *
 def Run(br: BaseRobot):
     br.moveLeftAttachmentMotorForMillis(millis=500, speed=250)
     br.driveForDistance(600, 200)
-    br.turnForAngle(90, speed=100)
+    br.turnForAngle(90, speed=400)
     
 
 
