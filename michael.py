60f3059ed646b8b6c442204a858a652c5a3cacb7
friesfortwoninty-nine <michaelzhang9946@gmail.com>
2026-07-11 20:43:46 -0700

    Update michael.py

--- michael.py
+++ michael.py
@@ -3,26 +3,26 @@ from base_robot import *
 
 
 def Run(br: BaseRobot):
-    br.moveLeftAttachmentMotorForMillis(millis=500, speed=250)
-    br.driveForDistance(600, 200)
     
+    br.moveLeftAttachmentMotorForMillis(millis=1000, speed=250)
+    
+    br.driveForDistance(610, 200)
+    
+    br.moveLeftAttachmentMotorForMillis(millis=1000, speed=-250)
+
+    br.turnForAngle(angle=30, speed=200, then=Stop.BRAKE, gyro=True, accel=TURN_ACCEL, decel=TURN_DECEL,)
+
+    br.driveForDistance(50, 200)
+    
+    br.turnForAngle(angle=-30, speed=200, then=Stop.BRAKE, gyro=True, accel=TURN_ACCEL, decel=TURN_DECEL,)
+
+    br.driveForDistance(-610, 200)
+
 
     hub = PrimeHub()
-
-    while True:
-        br.drive(120, 0)
-        wait(2000)
-
-        br.drive(120, 150)
-        wait(2000)
-
-        br.drive(120, -150)
-        wait(2000)
-
 
 
 if __name__ == "__main__":
     br = BaseRobot()
     Run(br)
-
 
