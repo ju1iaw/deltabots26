b2171ccfb10499a3fb1ef70d989da83b5eec3dc9
tls123 <alyssa.w.0605@gmail.com>
2026-07-11 20:48:13 -0700

    updated alyssa.py

--- alyssa.py
+++ alyssa.py
@@ -1,18 +1,16 @@
 from base_robot import *
 
-
-
 def Run(br: BaseRobot):
-    br.driveForDistance(325, 200)
-    br.turnForAngle(92, 200)
-    br.driveForDistance(700, 200)
-    br.turnForAngle(92, 200)
-    br.driveForDistance(250, 200)
-    br.moveRightAttachmentMotorForMillis(500, 200)
-    br.driveForDistance(-200, 200)
-    br.turnForAngle(-92, 200)
-    br.driveForDistance(-700, 200)
-    br.turnForAngle(-92, 200)
+
+    br.driveForDistance(325, 300)
+    br.turnForAngle(92, 600)
+    br.driveForDistance(700, 300)
+    br.turnForAngle(92, 600)
+    br.driveForDistance(250, 300)
+    br.moveRightAttachmentMotorForMillis(500, 300)
+    br.driveForDistance(-200, 500)
+    br.turnForAngle(-120, 600)
+    br.driveForDistance(-900, 500)
 
 if __name__ == "__main__":
     br = BaseRobot()
