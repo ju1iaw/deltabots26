08b8ce1b2e9a07ee079ac37c707774365ab52bfc
DelinaChen <chen.delina7@gmail.com>
2026-07-19 09:28:40 -0700

    Update delina.py

--- delina.py
+++ delina.py
@@ -2,10 +2,16 @@ from base_robot import *
 
 
 def Run(br: BaseRobot):
-    # br.moveLeftAttachmentMotorForMillis(millis=500, speed=250)
-    # br.driveForDistance(600, 200)
-    # br.turnForAngle(-90, 200)
-    br.stop_line(200, 10)
+    br.driveForDistance(330, 200)
+    br.turnForAngle(90, 200)
+    br.driveForDistance(430, 200)
+    br.turnForAngle(-75, 200)
+    br.driveForDistance(40, 200)
+    br.moveRightAttachmentMotorForMillis(300, -200)
+    br.turnForAngle(20, 200)
+    br.driveForDistance(-300, 200)
+    br.turnForAngle(-130, 200)
+    br.driveForDistance(400, 200)
 
 
 if __name__ == "__main__":
