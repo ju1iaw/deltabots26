02093f0ea1718166f1f6ea363ae631d13a0a2a56
tls123 <alyssa.w.0605@gmail.com>
2026-07-19 10:24:16 -0700

    Update alyssa.py

--- alyssa.py
+++ alyssa.py
@@ -1,8 +1,8 @@
 from base_robot import *
-
+"""
 def Run(br: BaseRobot):
 
-    br.driveForDistance(325, 300)
+   br.driveForDistance(325, 300)
     br.turnForAngle(92, 600)
     br.driveForDistance(700, 300)
     br.turnForAngle(92, 600)
@@ -11,9 +11,15 @@ def Run(br: BaseRobot):
     br.driveForDistance(-200, 500)
     br.turnForAngle(-120, 600)
     br.driveForDistance(-900, 500)
+"""
+
+def Run2(br: BaseRobot):
+    br.driveForDistance(700, 300)
+    br.stop_line(speed = 100, reflectivity = 10, tolerance = 5)
+    br.align_line(reflectivity = 10, tolerance = 5)
 
 if __name__ == "__main__":
     br = BaseRobot()
-    Run(br)
+    Run2(br)
 
 
