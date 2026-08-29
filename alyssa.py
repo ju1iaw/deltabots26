5b37d42c137ddebc470601d87b246d222e8e97a1
tls123 <alyssa.w.0605@gmail.com>
2026-08-28 21:39:38 -0700

    I FORGOT TO PUSH THIS I THINK

--- alyssa.py
+++ alyssa.py
@@ -11,15 +11,44 @@ def Run(br: BaseRobot):
     br.driveForDistance(-200, 500)
     br.turnForAngle(-120, 600)
     br.driveForDistance(-900, 500)
-"""
 
 def Run2(br: BaseRobot):
     br.driveForDistance(700, 300)
     br.stop_line(speed = 100, reflectivity = 10, tolerance = 5)
-    br.align_line(reflectivity = 10, tolerance = 5)
+    br.align_line(reflectivity = 50, tolerance = 10)
 
+
+def Run3(br: BaseRobot):
+    br.moveRightAttachmentMotorForMillis(400, 200)
+    #br.driveForDistance(200, 300)
+    #br.moveRightAttachmentMotorForMillis(-50, 300)
+    #br.driveForDistance(-200, 300)
+
+"""
+
+def colorsensorthingy(br: BaseRobot):
+    x = 0
+    if br.colorSensorRight.reflection() <= 27:
+        x = 1
+        br.moveRightAttachmentMotorForMillis(1000, -200)
+    br.driveForDistance(-200, 200)
+    if x == 1:
+        br.moveRightAttachmentMotorForMillis(700, 200)
+    br.turnForAngle(89, 200)
+    br.driveForDistance(87, 200)
+    br.turnForAngle(-90, 200)
+    br.driveForDistance(205, 200)
+
+def Run4(br: BaseRobot):
+    br.driveForDistance(375, 300)
+    for i in range(3):
+        colorsensorthingy(br)
+
+
+
+    
 if __name__ == "__main__":
     br = BaseRobot()
-    Run2(br)
+    Run4(br)
 
 
