5f55a6bc0a5e591559588e61761f93baa6a722ec
DelinaChen <chen.delina7@gmail.com>
2026-09-05 19:14:53 -0700

    I finished mission 3 and 4.

--- delina.py
+++ delina.py
@@ -14,8 +14,88 @@ def Run(br: BaseRobot):
     br.driveForDistance(400, 200)
 
 
+def align(br: BaseRobot):
+    br.driveForDistance(650, 200)
+    br.stop_line(200, 20, tolerance=5)
+    br.align_line(
+        100, 25, tolerance=5, forward_speed=30, max_turn_rate=40, kp=0.9
+    )
+
+
+def Run2(br: BaseRobot):
+    br.driveForDistance(650, 200)
+    br.turnForAngle(-100, 200)
+    br.driveForDistance(100, 200)
+    br.turnForAngle(-40, 200)
+    br.moveRightAttachmentMotorForMillis(1000, 200)
+    br.driveForDistance(-200, 200)
+    br.turnForAngle(100, 200)
+    br.driveForDistance(600, -200)
+
+
+def test(br: BaseRobot):
+    for i in range(100):
+        print(br.colorSensorLeft.reflection())
+
+
+def rock(br: BaseRobot):
+    if br.colorSensorLeft.reflection() < 25:
+        br.moveRightAttachmentMotorForMillis(1000, 200)
+        br.driveForDistance(-200, 200)
+        br.moveRightAttachmentMotorForMillis(1000, -200)
+        br.turnForAngle(90, 200)
+        br.driveForDistance(75, 200)
+        br.turnForAngle(-90, 200)
+        br.driveForDistance(200, 200)
+    else:
+        br.driveForDistance(-200, 200)
+        br.turnForAngle(90, 200)
+        br.driveForDistance(75, 200)
+        br.turnForAngle(-90, 200)
+        br.driveForDistance(200, 200)
+
+
+def therock(br: BaseRobot):
+    br.driveForDistance(390, 200)
+    for i in range(3):
+        rock(br)
+
+
+def mission3(br: BaseRobot):
+    br.driveForDistance(431.8, 200)
+    br.driveForDistance(-431.8, 200)
+    """
+    while Button.LEFT not in pressed:
+        pressed = br.hub.buttons.pressed()
+    br.turnForAngle(50, 200)
+    br.driveForDistance(300, 200)
+    br.turnForAngle(-50, 200)
+    br.driveForDistance(200, 200)
+    """
+
+
+def mission4_5(br: BaseRobot):
+    br.moveLeftAttachmentMotorForMillis(3000, -200)
+
+    br.driveForDistance(698.5, 200)
+    br.turnForAngle(-38, 200)
+    br.driveForDistance(110, 200)
+    br.moveLeftAttachmentMotorForMillis(1000, 200)
+    br.turnForAngle(38, 200)
+    br.moveRightAttachmentMotorForMillis(1000, 200)
+
+
+"""   
+    while Button.LEFT not in pressed:
+        pressed = br.hub.buttons.pressed()
+    br.turnForAngle(50, 200)
+    br.driveForDistance(300, 200)
+    br.turnForAngle(-50, 200)
+    br.driveForDistance(200, 200)
+    """
+
+
 if __name__ == "__main__":
     br = BaseRobot()
-    Run(br)
-
+    mission4_5(br)
     br.stop()
